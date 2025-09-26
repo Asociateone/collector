22d96d6694b38f224a0ae5fe01544c6b36c31435
rainaldo <rainaldo@gutsgusto.com>
2025-09-26 22:08:15 +0200

    Wip adding correct item

--- app/src/main/java/com/example/collecter/ui/composables/partials/auth/AuthenticationNavBar.kt
+++ app/src/main/java/com/example/collecter/ui/composables/partials/auth/AuthenticationNavBar.kt
@@ -1,13 +1,6 @@
 package com.example.collecter.ui.composables.partials.auth
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -18,11 +11,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.collecter.enums.AuthNavigation
-import com.example.collecter.enums.UiState
 
 
 @Composable
@@ -30,14 +21,14 @@ fun AuthenticationNavBar(modifier: Modifier = Modifier, navController: NavHostCo
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
     NavigationBar(
-        modifier = modifier,
+        modifier = Modifier.fillMaxWidth().then(modifier),
         containerColor = Color.Transparent,
         contentColor = Color.Black,
     ) {
         if (navBackStackEntry?.destination?.route.toString() != AuthNavigation.SignIn.name) {
             IconButton(
                 onClick = { navController.navigateUp() },
-                modifier = Modifier.align(Alignment.CenterVertically) // Added for vertical alignment
+                modifier = Modifier.align(Alignment.CenterVertically)
             ) {
                 Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
             }
