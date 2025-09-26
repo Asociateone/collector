a9b2b62a991d1a6112fa1a1faadae15c10b06eb9
rainaldo <rainaldo@gutsgusto.com>
2025-09-26 22:13:43 +0200

    switch screens

--- app/src/main/java/com/example/collecter/ui/composables/partials/auth/AuthenticationNavBar.kt
+++ app/src/main/java/com/example/collecter/ui/composables/partials/auth/AuthenticationNavBar.kt
@@ -3,35 +3,34 @@ package com.example.collecter.ui.composables.partials.auth
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.NavigationBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.collecter.enums.AuthNavigation
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AuthenticationNavBar(modifier: Modifier = Modifier, navController: NavHostController): Unit {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-    NavigationBar(
-        modifier = Modifier.fillMaxWidth().then(modifier),
-        containerColor = Color.Transparent,
-        contentColor = Color.Black,
-    ) {
-        if (navBackStackEntry?.destination?.route.toString() != AuthNavigation.SignIn.name) {
-            IconButton(
-                onClick = { navController.navigateUp() },
-                modifier = Modifier.align(Alignment.CenterVertically)
-            ) {
-                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+    if (navBackStackEntry?.destination?.route.toString() != AuthNavigation.SignIn.name) {
+        CenterAlignedTopAppBar(
+            title = { },
+            modifier = modifier.fillMaxWidth(),
+            navigationIcon = {
+                IconButton(
+                    onClick = { navController.navigateUp() },
+                ) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                }
             }
-        }
+        )
     }
 }
